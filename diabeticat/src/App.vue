c4c2a24121ace542eacb0edbe183f6e2f058d0ce
Philipp Schmidt <phi.schmidt@ostfalia.de>
2023-03-24 15:45:41 +0100

    Added Vuetify

--- diabeticat/src/App.vue
+++ diabeticat/src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-app-bar
+      color="primary"
+      app
+      dark
+    >
+    <v-icon icon="home" />
+      <v-toolbar-title>App Bar!</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container
+        >Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Quis blanditiis beatae praesentium repudiandae
+        sunt officiis, vero maxime officia obcaecati nisi
+        excepturi. Sit laboriosam provident laborum? Rem
+        recusandae ea possimus illo.</v-container
+      >
+    </v-main>
+    <v-bottom-navigation
+      v-model="value"
+      app
+    >
+      <v-btn
+        value="music"
+        height="100%"
+      >
+        <span>Music</span>
+        <v-icon>mdi-music</v-icon>
+      </v-btn>      <v-btn
+        value="books"
+        height="100%"
+      >
+        <span>Books</span>
+        <v-icon>mdi-book</v-icon>
+      </v-btn>      <v-btn
+        value="photos"
+        height="100%"
+      >
+        <span>Photos</span>
+        <v-icon>mdi-image</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
