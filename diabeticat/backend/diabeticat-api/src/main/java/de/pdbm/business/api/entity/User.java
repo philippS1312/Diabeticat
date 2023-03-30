a0a6fb2aa5ac8602ce2bcf215dc2c5961c014d31
Oliver Wanzek <o.wanzek@ostfalia.de>
2023-03-30 23:36:17 +0200

    updated User Entity class

--- diabeticat/backend/diabeticat-api/src/main/java/de/pdbm/business/api/entity/User.java
+++ diabeticat/backend/diabeticat-api/src/main/java/de/pdbm/business/api/entity/User.java
@@ -5,14 +5,14 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
- * User entity from database. Holds customer data.
+ * User entity from database. Holds user data.
  */
 @Entity
 public class User {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private int user_id;
+        private int id;
 
         private String username;
         private String email;
@@ -21,19 +21,14 @@ public class User {
         public User() {
 
         }
-        public User(int user_id, String username, String email, String password) {
-                this.user_id = user_id;
+        public User(String username, String email, String password) {
                 this.username = username;
                 this.email = email;
                 this.password = password;
         }
 
-        public int getUser_id() {
-                return user_id;
-        }
-
-        public void setUser_id(int user_id) {
-                this.user_id = user_id;
+        public int getId() {
+                return id;
         }
 
         public String getUsername() {
@@ -63,7 +58,7 @@ public class User {
         @Override
         public String toString() {
                 return "User{" +
-                        "user_id=" + user_id +
+                        "user_id=" + id +
                         ", username='" + username + '\'' +
                         ", email='" + email + '\'' +
                         ", password='" + password + '\'' +
