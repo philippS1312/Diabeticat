f094cc30c0d33c037b1c830c68bb83397df62de9
MelvinB15 <m.benzer01@outlook.de>
2023-03-31 12:27:50 +0200

    MeasurementService and PetService created

--- diabeticat/backend/diabeticat-api/src/main/java/de/pdbm/business/api/boundary/MeasurementService.java
+++ diabeticat/backend/diabeticat-api/src/main/java/de/pdbm/business/api/boundary/MeasurementService.java
@@ -1,4 +1,35 @@
 package de.pdbm.business.api.boundary;
 
+
+import de.pdbm.business.api.entity.MeasurementData;
+import de.pdbm.business.api.entity.Pet;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
 public class MeasurementService {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public MeasurementData getMeasurementDataById(int measurementId){ return em.find(MeasurementData.class, measurementId);}
+
+    public List<MeasurementData> getMeasurementDataByPet(Pet pet){
+        return em.createQuery("SELECT m FROM MeasurementData m WHERE m.pet.id = :id", MeasurementData.class).setParameter("id", pet.getId()).getResultList();
+    }
+
+    public void deleteMeasurementData(MeasurementData measurementData){
+        em.remove(measurementData);
+    }
+
+    /**
+     * Save Data to the DataBase
+     * @param measurementData
+     */
+    public void persistMeasurement(MeasurementData measurementData) {
+        em.persist(measurementData);
+    }
 }
