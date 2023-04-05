fc0570b6818d78c170399f2beae1a24ee7ba8565
oliverw77 <74662347+oliverw77@users.noreply.github.com>
2023-04-05 12:15:54 +0200

    Delete diabeticat/backend directory

--- diabeticat/backend/clean-up-jar.sh
+++
@@ -1,6 +0,0 @@
-
-#Look for open wildfly instances and close them:
-wmic Path win32_process Where "CommandLine Like '%dvd-rental-customer-bootable.jar%'" Call Terminate
-wmic Path win32_process Where "CommandLine Like '%dvd-rental-film-bootable.jar%'" Call Terminate
-wmic Path win32_process Where "CommandLine Like '%dvd-rental-store-bootable.jar%'" Call Terminate
-wmic Path win32_process Where "CommandLine Like '%dvd-rental-ui-bootable.jar%'" Call Terminate
