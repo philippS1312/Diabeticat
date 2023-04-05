c76867600dd0bf11f6d54e4be38b7d71019d5e74
oliverw77 <74662347+oliverw77@users.noreply.github.com>
2023-04-05 12:15:27 +0200

    Delete diabeticat/backend/diabeticat-api directory

--- diabeticat/backend/diabeticat-api/src/main/java/de/pdbm/config/JaxRsActivator.java
+++
@@ -1,9 +0,0 @@
-package de.pdbm.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/resources")
-public class JaxRsActivator extends Application {
-
-}
