6f92e782236929ded12e3cde64a1fb6f21fa1ef4
Oliver Wanzek <o.wanzek@ostfalia.de>
2023-04-06 11:17:59 +0200

    Created Pet API Endpoints

--- diabeticat-backend/DiabeticatAPI/API/Pet.py
+++ diabeticat-backend/DiabeticatAPI/API/Pet.py
@@ -1,6 +1,49 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+import mysql.connector
 
-bye_router = APIRouter()
-@bye_router.get('/bye')
-def bye():
-    return {"Schönen":"Tag Noch"}
+pet_router = APIRouter()
+
+def connectDB():
+    mydb = mysql.connector.connect(
+        host="sql7.freemysqlhosting.net",
+        user="sql7609007",
+        password="xSVyvqUxhW",
+        database="sql7609007"
+    )
+    return mydb
+
+@pet_router.post("/createPet")
+async def createPet(pet: Request):
+    req_pet = await pet.json()
+    name = req_pet["name"]
+    type = req_pet["type"]
+    birthday = req_pet["birthday"]
+    userId = req_pet["userId"]
+
+    mydb=connectDB()
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO Pet (name, type, birthday, userId) VALUES (%s, %s, %s, %s)"
+    val = (name, type, birthday, userId)
+    mycursor.execute(sql,val)
+
+    mydb.commit()
+    print(mycursor.rowcount, "New Pet inserted!")
+
+    return {"Succuess":True}
+
+@pet_router.post("/deletePet")
+async def deletePet(pet: Request):
+    req_pet = await pet.json()
+    petId = req_pet["id"]
+
+    mydb=connectDB()
+    mycursor = mydb.cursor()
+
+    sql = "DELETE FROM Pet WHERE petId='" + petId + "'"
+    mycursor.execute(sql)
+
+    mydb.commit()
+    print("Pet sucessfully deleted!")
+
+    return {"Succuess":True}
--- diabeticat-backend/DiabeticatAPI/main.py
+++ diabeticat-backend/DiabeticatAPI/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from API.User import hello_router
-from API.Pet import bye_router
+from API.Pet import pet_router
 
 app = FastAPI()
 
 app.include_router(hello_router)
-app.include_router(bye_router)
+app.include_router(pet_router)
 
 
 
