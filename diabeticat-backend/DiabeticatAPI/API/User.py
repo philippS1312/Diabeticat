d0b6860895b5bc5dcaaa1d439402c0346ef4aaff
MelvinB15 <m.benzer01@outlook.de>
2023-04-06 11:36:45 +0200

    Created User login

--- diabeticat-backend/DiabeticatAPI/API/User.py
+++ diabeticat-backend/DiabeticatAPI/API/User.py
@@ -1,6 +1,57 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, HTTPException
+import json
+import mysql.connector
 
-hello_router = APIRouter()
-@hello_router.get('/hello')
-def hello():
-    return {"Ping":"Pong"}
+user_router = APIRouter()
+
+def connectDB():
+    mydb = mysql.connector.connect(
+        host="sql7.freemysqlhosting.net",
+        user="sql7609007",
+        password="xSVyvqUxhW",
+        database="sql7609007"
+    )
+    return mydb
+
+
+
+@user_router.post('/login')
+async def login(input: Request):
+    req = await input.json()
+    mydb = connectDB()
+    if "username" in req and "password" in req:
+        if req["username"] is not None and req["password"] is not None:
+            username = req["username"]
+            password = req["password"]
+            mydb = connectDB()
+            mycursor = mydb.cursor()
+            mycursor.execute("SELECT * FROM User where username ='" + username + "' and password='"+password+"'")
+            col_names = [col[0] for col in mycursor.description]
+            myresult = mycursor.fetchall()
+
+            result_list = []
+            for row in myresult:
+                result_dict = {}
+                for i in range(len(col_names)):
+                    result_dict[col_names[i]] = row[i]
+                result_list.append(result_dict)
+
+            result_json = json.dumps(result_list)[1:-1]
+            StrinJson= str(result_json)
+            returnJson= json.loads(StrinJson)
+
+            return returnJson
+        else:
+            raise HTTPException(status_code=404, detail="Username or Password Values not found")
+    else:
+        raise HTTPException(status_code=404, detail="Username or Password Keys not found")
+
+
+
+
+
+
+
+
+
+
