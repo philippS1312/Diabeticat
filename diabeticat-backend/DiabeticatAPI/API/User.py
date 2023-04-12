387d930de9a12c4569ac3c3acab42eb39badf067
MelvinB15 <m.benzer01@outlook.de>
2023-04-12 14:32:54 +0200

    CreateUser Endpoint

--- diabeticat-backend/DiabeticatAPI/API/User.py
+++ diabeticat-backend/DiabeticatAPI/API/User.py
@@ -4,14 +4,31 @@ import mysql.connector
 
 user_router = APIRouter()
 
-def connectDB():
-    mydb = mysql.connector.connect(
-        host="sql7.freemysqlhosting.net",
-        user="sql7609007",
-        password="xSVyvqUxhW",
-        database="sql7609007"
-    )
-    return mydb
+@user_router.post('/createUser')
+async def createUser(input: Request):
+    req = await input.json()
+    mydb = connectDB()
+    mycursor = mydb.cursor()
+    if "username" in req and "password" in req and "email" in req :
+        if req["username"] is not None and req["password"] is not None and req["email"] is not None:
+            username = req["username"]
+            password = req["password"]
+            email = req["email"]
+
+            sql = "INSERT INTO User (username, password, email) VALUES (%s, %s, %s)"
+            val = (username, password, email)
+            mycursor.execute(sql, val)
+
+            mydb.commit()
+
+            print(mycursor.rowcount, "New User inserted!")
+
+            return {"Succuess": True}
+
+        else:
+            raise HTTPException(status_code=404, detail="Username, Password or Email Values not found")
+    else:
+        raise HTTPException(status_code=404, detail="Email, Username or Password Keys not found")
 
 
 
@@ -46,7 +63,14 @@ async def login(input: Request):
     else:
         raise HTTPException(status_code=404, detail="Username or Password Keys not found")
 
-
+def connectDB():
+    mydb = mysql.connector.connect(
+        host="sql7.freemysqlhosting.net",
+        user="sql7609007",
+        password="xSVyvqUxhW",
+        database="sql7609007"
+    )
+    return mydb
 
 
 
